d435d00da208db6a97493021c6affdc20c423bb1
WKMoon <wkmoon.tech@gmail.com>
2021-09-01 20:49:17 +0900

    모듈화

--- addressbook/index.js
+++ addressbook/index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var mongoose = require('mongoose');
 var bodyParser = require('body-parser');
+var methodOverride = require('method-override');
 var app = express();
 
 //DB setting
@@ -23,41 +24,12 @@ app.set('view engine', 'ejs');
 app.use(express.static(__dirname+'/public'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
-
-//DB schema
-var contactSchema = mongoose.Schema({
-  name:{type:String, required:true, unique:true},
-  email:{type:String},
-  phone:{type:String}
-});
-var Contact = mongoose.model('contact', contactSchema);
+app.use(methodOverride('_method'));
 
 //Routes
-//Home
-app.get('/', function(req,res){
-  res.redirect('/contacts');
-});
+app.use('/', require('./routes/home'));
+app.use('/contacts', require('./routes/contacts'));
 
-//Contacts - Index
-app.get('/contacts', function(req,res){
-  Contact.find({}, function(err,contacts){
-    if(err) return res.json(err);
-    res.render('contacts/index', {contacts:contacts});
-  });
-});
-
-//Contacts - New
-app.get('/contacts/new', function(req,res){
-  res.render('contacts/new');
-});
-
-//Contacts - create
-app.post('/contacts', function(req,res){
-  Contact.create(req.body, function(err,contact){
-    if(err) return res.json(err);
-    res.redirect('/contacts');
-  });
-});
 
 var port = 3000;
 app.listen(port, function(){
